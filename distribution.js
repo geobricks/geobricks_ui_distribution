e2b6cbdaebba87d2c88d9130959e6a835dacfcf7
simonefenix <simone.murzilli@fao.org>
2015-05-08 17:06:50 +0200

    updated template

--- distribution.js
+++ distribution.js
@@ -1,15 +1,17 @@
 define([
     //'jquery',
-    'mustache',
+    'handlebars',
     'text!geobricks_ui_distribution/html/template.html',
     'i18n!geobricks_ui_distribution/nls/translate',
+    'text!geobricks_ui_distribution/config/data.json',
     'fenix-map',
     'highcharts',
     'bootstrap'], function (
     //$,
-    Mustache,
+    Handlebars,
     templates,
-    translate
+    translate,
+    configData
 ) {
     'use strict';
 
@@ -22,6 +24,13 @@ define([
             placeholder: 'main_content_placeholder',
             template_id: 'map',
 
+            datatype_dd_id: 'ghg_spatial_browse_datatype',
+            product_dd_id: 'ghg_spatial_browse_product',
+            layer_dd_id: 'ghg_spatial_browse_layer',
+            date_panel_id: 'ghg_spatial_browse_date_panel',
+            date_dd_id: 'ghg_spatial_browse_date',
+            map_id: 'ghg_spatial_browse_map',
+
             areas: {
                 query: "SELECT adm0_code, adm0_name FROM spatial.gaul0_2015_4326 WHERE disp_area = 'NO' ORDER BY adm0_name"
             },
@@ -30,78 +39,48 @@ define([
             m: null,
             l: null,
 
-            l_gaul0_highlight: null
+            l_gaul0_highlight: null,
+
+
+            // caching layers
+            cached_layers:null
         }
     }
 
     GEOBRICKS_UI_DISTRIBUTION.prototype.init = function(config) {
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
+        this.CONFIG.data = $.parseJSON(configData);
+        this.render();
+    };
 
-        var template = $(templates).filter('#' + this.CONFIG.template_id).html();
-        $('#' + this.CONFIG.placeholder).html(templates);
+    GEOBRICKS_UI_DISTRIBUTION.prototype.render = function() {
+        /* Load template. */
+        var source =  $(templates).filter('#main_structure').html();
+        var template = Handlebars.compile(source);
+        var dynamic_data = {
+            title: translate.ghg_spatial_browse,
+            select_a_datatype: translate.select_a_datatype,
+            select_a_product: translate.select_a_product,
+            select_a_layer: translate.select_a_layer,
+            show_layer: translate.show_layer,
+            select_date: translate.select_date
+        };
+        var html = template(dynamic_data);
+        $('#' + this.CONFIG.placeholder).html(html);
 
-        this.build_dropdown_products('pgeo_dist_prod')
+        // initialize dropdown
+        $('#' + this.CONFIG.product_dd_id).chosen({width: '100%'});
+        $('#' + this.CONFIG.layer_dd_id).chosen({width: '100%'});
+        $('#' + this.CONFIG.date_dd_id).chosen({width: '100%'});
 
-        //this.build_dropdown_gaul('pgeo_dist_areas')
+        // buid_datatype_dropdown
+        this.build_datatype_dropdown(this.CONFIG.datatype_dd_id, this.CONFIG.data);
 
-        // build map
-        this.build_map('pgeo_dist_map')
-        var _this = this;
-        $("#pgeo_dist_export_button").bind( "click", function() {
-            var areas = $("#pgeo_dist_areas_select").chosen().val();
-            var l = $("#pgeo_dist_layers_select").chosen().val();
-            var layers = []
-            for( var i=0; i < l.length; i++) {
-                var layer = JSON.parse(l[i])
-                layers.push({
-                    "workspace": layer.workspace,
-                    "layerName": layer.layerName,
-                    "datasource": layer.datasource
-                })
-            }
-            var email_address = $("#pgeo_dist_email_address").val();
-            _this.export_layers(layers, areas, email_address)
-        });
-    }
+        // buold map
+        this.build_map(this.CONFIG.map_id);
 
-    GEOBRICKS_UI_DISTRIBUTION.prototype.build_dropdown_products = function(id) {
-        var url = this.CONFIG.url_d3s_metadata_resource.replace("{{UID}}", "layer_products");
-        var _this = this;
-        $.ajax({
-            type : 'GET',
-            url : url,
-            success : function(response) {
-                response = (typeof response === 'string')? $.parseJSON(response): response;
-                var dropdowndID = id + "_select";
-                var html = '<select id="'+ dropdowndID+'" style="width:100%;">';
-                html += '<option value=""></option>';
-                for(var i=0; i < response.data.length; i++) {
-                    html += '<option value="' + response.data[i].code + '">' + response.data[i].title["EN"] + '</option>';
-                }
-                html += '</select>';
 
-                $('#' + id).empty();
-                $('#' + id).append(html);
-
-                try {
-                    $('#' + dropdowndID).chosen(
-                        {
-                            disable_search_threshold:6,
-                            width: '100%',
-                            placeholder_text_single: "Select a product"
-                        });
-                }  catch (e) {}
-
-                $( "#" + dropdowndID ).change(function () {
-                    _this.build_dropdown_layers("pgeo_dist_layers_select",$(this).val())
-                });
-            },
-            error : function(err, b, c) {}
-        });
-    }
-
-    GEOBRICKS_UI_DISTRIBUTION.prototype.build_dropdown_layers = function(id, product) {
-        $("#" + id).empty()
+/*        var _this =this;
         var request_filter = {
             "meContent.resourceRepresentationType" : {
                 "enumeration" : ["geographic"]
@@ -109,9 +88,94 @@ define([
             "meContent.seCoverage.coverageSectors" : {
                 "codes" : [
                     {
-                        "uid" : "layer_products",
+                        "uid" : "layers_products",
                         "version" : "1.0",
-                        "codes" : [product]
+                        "codes" : ['gfed4_burnedareas_otherforest']
+                    }
+                ]
+            }
+        }
+        var url = this.CONFIG.url_d3s_resources_find + "?" + this.CONFIG.url_d3s_resources_find_order_by_date_parameters;
+        console.log(url);
+        $.ajax({
+            type: 'POST',
+            url: url,
+            contentType: "application/json",
+            dataType: 'json',
+            data: JSON.stringify(request_filter),
+            crossDomain: true,
+            success : function(response) {
+                console.log(response);
+                _this.build_dates_dd(_this.CONFIG.date_dd_id, response)
+            },
+            error : function(err, b, c) {}
+        });*/
+    };
+
+    GEOBRICKS_UI_DISTRIBUTION.prototype.build_datatype_dropdown = function(id, data) {
+        $('#' + id).html(this.buid_dd_html(data));
+        $('#' + id).chosen({
+            disable_search_threshold: 7,
+            width: '100%'
+        });
+
+        var _this = this;
+        $("#" + id).change(function() {
+            var d = [];
+            if ($(this).find(":selected").data("value")) {
+                d = $(this).find(":selected").data("value").product;
+            }
+            _this.build_product_dropdown(_this.CONFIG.product_dd_id, d);
+        });
+    };
+
+    GEOBRICKS_UI_DISTRIBUTION.prototype.build_product_dropdown = function(id, data) {
+        console.log(id);
+        console.log(data);
+        $('#' + id).html(this.buid_dd_html(data));
+        $('#' + id).trigger("chosen:updated");
+        this.build_layer_dropdown(this.CONFIG.layer_dd_id, []);
+
+        var _this = this;
+        $("#" + id).unbind('change');
+        $("#" + id).change(function () {
+            console.log("TRIGGER PRODUCT");
+            var d = [];
+            if ($(this).find(":selected").data("value")) {
+                d = $(this).find(":selected").data("value").layer;
+            }
+            _this.build_layer_dropdown(_this.CONFIG.layer_dd_id, d);
+        });
+    };
+
+    GEOBRICKS_UI_DISTRIBUTION.prototype.build_layer_dropdown = function(id, data) {
+        $('#' + id).html(this.buid_dd_html(data));
+        $('#' + id).trigger("chosen:updated");
+        this.build_dates_dd(this.CONFIG.date_panel_id, this.CONFIG.date_dd_id, []);
+
+        var _this = this;
+        $("#" + id).unbind('change');
+        $("#" + id).chosen().change(function() {
+            console.log("TRIGGER LAYER");
+            var d = [];
+            if ($(this).find(":selected").data("value")) {
+                d = $(this).find(":selected").data("value").coverageSector;
+            }
+            _this.build_dates(_this.CONFIG.date_panel_id, _this.CONFIG.date_dd_id, d);
+        });
+    };
+
+    GEOBRICKS_UI_DISTRIBUTION.prototype.build_dates = function(panel_id, dd_id, coverageSectorCode) {
+        var request_filter = {
+            "meContent.resourceRepresentationType" : {
+                "enumeration" : ["geographic"]
+            },
+            "meContent.seCoverage.coverageSectors" : {
+                "codes" : [
+                    {
+                        "uid" : "layers_products",
+                        "version" : "1.0",
+                        "codes": [coverageSectorCode]
                     }
                 ]
             }
@@ -126,138 +190,97 @@ define([
             data: JSON.stringify(request_filter),
             crossDomain: true,
             success : function(response) {
-                response = (typeof response == 'string')? $.parseJSON(response): response;
-                var html = '<option value=""></option>';
-                for(var i=0; i < response.length; i++) {
-                    var dsd = JSON.stringify(response[i]["dsd"])
-                    //TODO: remove the replace. Do it with the DSD?
-                    html += "<option value='" + dsd + "'>" + response[i]['title'][_this.CONFIG.langISO2.toLocaleUpperCase()] + "</option>";
-                }
-                $('#' + id).append(html);
-                $('#' + id).trigger("chosen:updated");
+                // TODO build dropdown or display:none
+                console.log(response);
 
-                //$( "#" + id ).change(function () {
-                //    _this.on_change_layer(id)
-                //});
+                // caching layers
+                _this.CONFIG.cached_layers = response;
 
-                var select_all = id.replace("_select", "_select_all")
-                var deselect_all = id.replace("_select", "_deselect_all")
-                $("#"+ select_all ).bind("click", function() {
-                    $("#" + id + ">option").prop('selected', true);
-                    $('#' + id).trigger("chosen:updated");
-                    _this.on_change_layer(id)
-                });
-
-                $("#" + deselect_all).bind("click", function() {
-                    $("#" + id + ">option").prop('selected', false);
-                    $('#' + id).trigger("chosen:updated");
-                    _this.on_change_layer(id)
-                });
-
-                $( "#pgeo_dist_layers_add_layer").click(function () {
-                    _this.on_change_layer(id)
-                });
+                _this.build_dates_dd(panel_id, dd_id, response)
             },
-            error : function(err, b, c) {}
+            error : function(err, b, c) {
+                console.log("ERROR D3s");
+            }
         });
-    }
+    };
 
-    GEOBRICKS_UI_DISTRIBUTION.prototype.on_change_layer = function(id) {
-        var values = $("#" + id + ' option:selected');
-        if (this.CONFIG.l) {
-            this.CONFIG.m.removeLayer(this.CONFIG.l)
+    GEOBRICKS_UI_DISTRIBUTION.prototype.build_dates_dd = function(panel_id, dd_id, data) {
+        console.log(data);
+
+        var dates = [];
+        for (var i in data) {
+            try {
+                dates.push(new Date(data[i]["meContent"]["seCoverage"]["coverageTime"]["from"]).getFullYear());
+            }catch(e) {}
         }
 
-        if ( values.length > 0 ) {
-
-            if (values[0].value == '') {
-                values.splice(0, 1);
+        if (dates.length > 0) {
+            var html = ''
+            for(var i=0; i < dates.length; i++) {
+                html += '<option value="' + dates[i] + '">' + this.getLabel(dates[i]) + '</option>';
             }
-
-            if (values) {
-                var layer = {};
-                var l = JSON.parse(values[0].value);
-                var workspace = l.workspace
-                var layerName = l.layerName
-                layer.layers = workspace + ":" + layerName
-                layer.layertitle = values[0].text
-                layer.urlWMS = this.CONFIG.url_geoserver_wms
-                layer.opacity = '0.75';
-                layer.defaultgfi = true;
-                layer.openlegend = true;
-                layer.lang = "EN";
-
-                /* TODO: check if raster */
-                layer.customgfi = {
-                    content: {
-                        /* TODO: change style classes */
-                        EN: "<div class='fm-legend-layertitle'><b>"+ layer.layertitle +"</b><div class='fm-popup-join-content'>Pixel Value {{GRAY_INDEX}}</div></div>"
-                    },
-                    showpopup: true
-                }
-
-                this.CONFIG.l = new FM.layer(layer, {noWrap: true});
-                this.CONFIG.m.addLayer(this.CONFIG.l);
-            }
+            $('#' + dd_id).html(html);
+            $("#" + panel_id).show();
         }
-    }
+        else {
+            $('#' + dd_id).html('<option value="null">' + translate.please_select + '</option>');
+            $("#" + panel_id).hide();
+        }
+        $('#' + dd_id).trigger("chosen:updated");
 
-    GEOBRICKS_UI_DISTRIBUTION.prototype.build_dropdown_gaul = function(id) {
-        var query = this.CONFIG.areas.query;
-        var url = this.CONFIG.url_spatialquery_db_spatial + query;
         var _this = this;
-        $.ajax({
-            type : 'GET',
-            url : url,
-            success : function(response) {
-                response = (typeof response == 'string')? $.parseJSON(response): response;
-                var dropdowndID = id + "_select"
-                var html = '<select id="'+ dropdowndID+'"  multiple="" style="width:100%;">';
-                html += '<option value="world">World</option>';
-                for(var i=0; i < response.length; i++) {
-                    html += '<option value="' + response[i][0] + '">' + response[i][1] + '</option>';
-                }
-                html += '</select>';
+        $("#" + dd_id).unbind('change');
+        $("#" + dd_id).change(function(e, params) {
+            _this.showLayer();
+        });
+        this.showLayer();
+    };
 
-                $('#' + id).empty();
-                $('#' + id).append(html);
+    GEOBRICKS_UI_DISTRIBUTION.prototype.buid_dd_html = function(data) {
+        var html = '<option value="null">' + translate.please_select + '</option>';
+        for(var i=0; i < data.length; i++) {
+            html += '<option data-value='+ JSON.stringify(data[i]) + '>' + this.getLabel(data[i].title) + '</option>';
+        }
+        return html;
+    };
 
-                try {
-                    $('#' + dropdowndID).chosen(
-                        {
-                            disable_search_threshold:6,
-                            width: '100%',
-                            placeholder_text_multiple: "Select a country"
-                        });
-                }  catch (e) {}
+    GEOBRICKS_UI_DISTRIBUTION.prototype.showLayer = function() {
+        console.log("showLayer");
 
-                $( "#" + dropdowndID ).change(function () {
-                    var values = $(this).val()
-                    if ( values ) {
-                        if ( $(this).val() != "world") {
-                            var codes = _this.get_string_codes(values, "'")
-                            _this.CONFIG.l_gaul0_highlight.layer.cql_filter = "adm0_code IN (" + codes + ")";
-                            _this.CONFIG.l_gaul0_highlight.redraw()
-                            _this.zoom_to(_this.get_string_codes(values, ""))
+        var layer = $("#" + this.CONFIG.layer_dd_id).find(":selected").data("value");
+        var year = $("#" + this.CONFIG.date_dd_id).chosen().val();
+
+        var layerDef = null;
+        if (layer) {
+            var cached_layers = this.CONFIG.cached_layers;
+            if (cached_layers) {
+                if (cached_layers.length > 0) {
+                    console.log("HERE");
+                    if (year) {
+                        var fromDate = new Date(year, 0, 1).getTime();
+                        for (var i in cached_layers) {
+                            if (cached_layers[i].meContent.seCoverage.coverageTime.from == fromDate){
+                                layerDef = cached_layers[i];
+                                break;
+                            }
                         }
                     }
-                    else {
-                        _this.CONFIG.l_gaul0_highlight.layer.cql_filter = "adm0_code IN ('0')";
-                        _this.CONFIG.l_gaul0_highlight.redraw()
-                        // TODO: reset zoom
-                    }
-                });
-            },
-            error : function(err, b, c) {}
-        });
-    }
+                }
+                else {
+                    // TODO: add check
+                    layerDef = cached_layers[0];
+                }
+            }
+        }
+        this.on_change_layer(layerDef)
+    };
 
     GEOBRICKS_UI_DISTRIBUTION.prototype.build_map = function(id) {
         var options = {
             plugins: { geosearch : false, mouseposition: false, controlloading : false, zoomControl: 'bottomright'},
             guiController: { overlay : true,  baselayer: true,  wmsLoader: false },
             gui: {disclaimerfao: true }
-        }
+        };
 
         var mapOptions = {
             zoomControl:false,
@@ -284,25 +307,40 @@ define([
         layer.zindex= 550;
         this.CONFIG.l_gaul0 = new FM.layer(layer, {noWrap : true});
         this.CONFIG.m.addLayer(this.CONFIG.l_gaul0);
-    }
+    };
 
-
-    GEOBRICKS_UI_DISTRIBUTION.prototype.get_string_codes = function(values, apex) {
-        var codes= ""
-        apex = typeof apex !== 'undefined' ? apex : "'";
-        for( var i=0; i < values.length; i++) {
-            codes += apex + values[i] + apex+","
+    GEOBRICKS_UI_DISTRIBUTION.prototype.on_change_layer = function(layerDef) {
+        if (this.CONFIG.l) {
+            this.CONFIG.m.removeLayer(this.CONFIG.l)
         }
-        return codes.substring(0, codes.length - 1);
-    }
 
-    GEOBRICKS_UI_DISTRIBUTION.prototype.get_string_uids = function(values) {
-        var codes= ""
-        for( var i=0; i < values.length; i++) {
-            codes += "" + values[i] +","
+        if (layerDef) {
+            var layer = {};
+            layer.layers = layerDef.dsd.workspace + ":" + layerDef.dsd.layerName;
+            layer.layertitle = layerDef.title.EN;
+            layer.urlWMS = this.CONFIG.url_geoserver_wms;
+            layer.opacity = '0.75';
+            layer.defaultgfi = true;
+            layer.openlegend = true;
+            layer.lang = "EN";
+
+            /* TODO: check if raster */
+            layer.customgfi = {
+                content: {
+                    /* TODO: change style classes */
+                    EN: "<div class='fm-legend-layertitle'><b>" + layer.layertitle + "</b><div class='fm-popup-join-content'>Pixel Value {{GRAY_INDEX}}</div></div>"
+                },
+                showpopup: true
+            }
+
+            this.CONFIG.l = new FM.layer(layer, {noWrap: true});
+            this.CONFIG.m.addLayer(this.CONFIG.l);
         }
-        return codes.substring(0, codes.length - 1);
-    }
+    };
+
+    GEOBRICKS_UI_DISTRIBUTION.prototype.getLabel = function(label) {
+        return translate[label]? translate[label]: label;
+    };
 
     return GEOBRICKS_UI_DISTRIBUTION;
 });
--- nls/translate.js
+++ nls/translate.js
@@ -1,7 +1,23 @@
 define({
     'root' : {
-        'boundaries' : 'Boundaries'
+        ghg_spatial_browse: "GHG Spatial Browse",
+        please_select: "Please Select...",
+        error: "Error",
+        select_a_datatype: "Select a Datatype",
+        select_a_product: "Select a Product",
+        select_a_layer: "Select a Layer",
+        select_a_country: "Select a Country",
+        select_date: "Select an year",
+        show_layer: "Show Layer",
 
+        // map
+        'boundaries' : 'Boundaries',
+
+        // data types
+        processed_activity_data: "Processed Activity Data",
+        processed_activity_data_description: "processed_activity_data_description",
+        parameters_emission_factors: "Parameters and emission factors",
+        parameters_emission_factors_description: "Parameters and emission factors"
     },
     'F' : true,
     'S' : true
