afa6fea3db58b73bfe9d5db90963f57e904283b3
simonefenix <simone.murzilli@fao.org>
2015-05-11 18:43:08 +0200

    fixed ui

--- nls/translate.js
+++ nls/translate.js
@@ -15,9 +15,66 @@ define({
 
         // data types
         processed_activity_data: "Processed Activity Data",
-        processed_activity_data_description: "processed_activity_data_description",
+        processed_activity_data_description: "",
         parameters_emission_factors: "Parameters and emission factors",
-        parameters_emission_factors_description: "Parameters and emission factors"
+        parameters_emission_factors_description: "",
+        ancillary_datasets: "Ancillary Datasets",
+        ancillary_datasets_description: "",
+        emission_estimates: "Emission Estimates",
+        emission_estimates_description: "",
+
+        // products
+        gfed4_burned_areas_yearly: "GFED4 Burned Areas (yearly)",
+        gfed4_burned_areas_yearly_description: "asd",
+        burned_dry_matter_yearly: "Burned Dry Matter (yearly)",
+        burned_dry_matter_yearly_description: "",
+        organic_soils_area: "Organic Soils Area",
+        organic_soils_area_description: "",
+
+        fuel_biomass_factors: "Fuel Biomass Factors",
+        fuel_biomass_factors_description: "",
+        emission_factors: "Emission Factors",
+        emission_factors_description: "",
+
+        climate: "Climate",
+        climate_description: "",
+        ecology: "Ecology",
+        ecology_description: "",
+        livestock: "Livestock",
+        livestock_description: "",
+        land_cover: "Land Cover",
+        land_cover_description: "",
+
+        faostat_fire_co2_emission_estimates_yearly: "FAOSTAT Fire CO2 Emission Estimates Yearly",
+        faostat_fire_co2_emission_estimates_yearly_description: "",
+        faostat_fire_n2o_emission_estimates_yearly: "FAOSTAT Fire N2O Emission Estimates Yearly",
+        faostat_fire_n2o_emission_estimates_yearly_description: "",
+        faostat_fire_ch4_emission_estimates_yearly: "FAOSTAT Fire CH4 Emission Estimates Yearly",
+        faostat_fire_ch4_emission_estimates_yearly_description: "",
+        faostat_cultivation_organic_soil_co2_emission_estimates: "FAOSTAT Cultivation Organic Soil CO2 Emission Estimates",
+        faostat_cultivation_organic_soil_co2_emission_estimates_description: "",
+        faostat_cultivation_organic_soil_n2o_emission_estimates: "FAOSTAT Cultivation Organic Soil N2O Emission Estimates",
+        faostat_cultivation_organic_soil_n2o_emission_estimates_description: "",
+
+        // layers
+        humid_tropical_forest: "Humid ropical Forest",
+        other_forest: "Other Forest",
+        closed_shrubland: "Closed Shrubland",
+        open_shrubland: "Open Shrubland",
+        woody_savanna: "Woody Savanna",
+        savanna: "Savanna",
+        grassland: "Grassland",
+        peatlands: "Peatlands",
+
+        organic_soils: "Organic Soils",
+        cropland_organic_soils: "Cropland Organic Soils",
+        grassland_organic_soils: "Grassland Organic Soils",
+        jrc_climate_zone_map: "JRC Climate Zone Map",
+        global_ecological_zones_gez2010: "Global Ecological Zones (GEZ2010)",
+        gridded_livestock_cattle: "Gridded Livestock Cattle",
+        gridded_livestock_sheep: "Gridded Livestock Sheep",
+        global_land_cover_glc2000:"Global Land Cover (GLC2000)"
+
     },
     'F' : true,
     'S' : true
